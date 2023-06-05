cddda66c818667daabe41d574f88c90e01e15920
Santiago Rivera <santiago1297@hotmail.es>
2023-06-05 14:46:53 -0500

    se suben primeros archivos

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+// import './App.css'
+import Header from "./components/Header";
+import { MovieList } from "./components/MovieList";
+
+const latestMovies = [
+  { name: "Avatar 2" },
+  { name: "Matrix 4" },
+  { name: "Thor" },
+];
+
+const childrenMovies = [{ name: "Minions" }, { name: "Alice in Wonderland" }];
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieList list={latestMovies} />
+      <MovieList list={childrenMovies} />
+    </div>
+  );
+}
+
+export default App;
